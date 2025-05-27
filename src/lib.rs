0ad8529ae1f3c67c356ba1fa04781ac8e930a183
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2025-05-27 23:19:51 +0200

    Expose Display in public API

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ mod utils;
 // Helper Methods
 pub use targets::get_all_targets;
 pub use targets::Target;
+pub use targets::{Display, Window};
 pub use utils::has_permission;
 pub use utils::is_supported;
 pub use utils::request_permission;
